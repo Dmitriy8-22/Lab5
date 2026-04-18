aa41373c4bc9ab8f9e73389acaa876b1f197c385
Dmitriy8-22 <dmitrijkiselev36@gmail.com>
2026-04-18 22:02:46 +0300

    Fix

--- banking/tests/test_account.cpp
+++ banking/tests/test_account.cpp
@@ -28,43 +28,33 @@ TEST(AccountTest, ChangeBalanceThrowsWhenNotLocked) {
 
 TEST(AccountTest, LockThrowsWhenAlreadyLocked) {
     Account acc(1, 1000);
-    
     EXPECT_NO_THROW(acc.Lock());
-    
     EXPECT_THROW(acc.Lock(), std::runtime_error);
-    
     EXPECT_NO_THROW(acc.ChangeBalance(100));
     EXPECT_EQ(acc.GetBalance(), 1100);
 }
 
 TEST(AccountTest, UnlockAllowsLockAgain) {
     Account acc(1, 1000);
-    
     acc.Lock();
-    acc.Unlock();  // Строка 22
-   
+    acc.Unlock();
     EXPECT_NO_THROW(acc.Lock());
 }
 
 TEST(AccountTest, MultipleLockUnlockCycles) {
     Account acc(1, 1000);
-    
     for (int i = 0; i < 3; i++) {
         acc.Lock();
         acc.ChangeBalance(100);
         acc.Unlock();
     }
-    
     EXPECT_EQ(acc.GetBalance(), 1300);
 }
 
 TEST(AccountTest, UnlockRequiresLockAgain) {
     Account acc(1, 1000);
-    
     acc.Lock();
     acc.ChangeBalance(100);
     acc.Unlock();
-    
-  
     EXPECT_THROW(acc.ChangeBalance(50), std::runtime_error);
 }
